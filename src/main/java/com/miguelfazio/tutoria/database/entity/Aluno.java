4794f2d62497201ba2afba3ea550989e5a2bb9f9
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-02-16 17:34:04 -0300

    Criação da Classe Aluno com seus getters e setters, também criado construtores com parametro e sem.

---
+++ src/main/java/com/miguelfazio/tutoria/database/entity/Aluno.java
@@ -0,0 +1,14 @@
+package com.miguelfazio.tutoria.database.entity;
+
+import lombok.Data;
+
+@Data
+public class Aluno {
+    private String nome;
+
+    public Aluno() {
+    }
+
+    public Aluno(String nome) {
+    }
+}
