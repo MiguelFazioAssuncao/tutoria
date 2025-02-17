af5436881e8bff61e2fdb93844066abb37040f3e
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-02-17 19:35:41 -0300

    Criação do AlunoService onde fica a lógica de cada endpoint

---
+++ src/main/java/com/miguelfazio/tutoria/service/AlunoService.java
@@ -0,0 +1,63 @@
+package com.miguelfazio.tutoria.service;
+
+import com.miguelfazio.tutoria.controller.CreateAlunoDTO;
+import com.miguelfazio.tutoria.controller.UpdateAlunoDTO;
+import com.miguelfazio.tutoria.database.entity.Aluno;
+import com.miguelfazio.tutoria.database.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class AlunoService {
+
+    private AlunoRepository alunoRepository;
+
+    @Autowired
+    public AlunoService(AlunoRepository alunoRepository) {
+        this.alunoRepository = alunoRepository;
+    }
+
+    public UUID createAluno(CreateAlunoDTO createAlunoDTO) {
+        var entity = new Aluno(createAlunoDTO.nome());
+        var userSaved = alunoRepository.save(entity);
+
+        return userSaved.getId();
+    }
+
+    public Optional<Aluno> getAlunoById(String alunoId) {
+        return alunoRepository.findById(UUID.fromString(alunoId));
+    }
+
+    public List<Aluno> listAlunos() {
+        return alunoRepository.findAll();
+    }
+
+    public void updateAlunoById(String alunoId, UpdateAlunoDTO updateAlunoDTO) {
+        var id = UUID.fromString(alunoId);
+        var alunoExists = alunoRepository.findById(id);
+
+        if (alunoExists.isPresent()) {
+            var aluno = alunoExists.get();
+
+            if (updateAlunoDTO.nome() != null) {
+                aluno.setNome(updateAlunoDTO.nome());
+            }
+
+            alunoRepository.save(aluno);
+        }
+    }
+
+    public void deleteById(String alunoId) {
+        var id = UUID.fromString(alunoId);
+        var alunoExists = alunoRepository.existsById(id);
+
+        if (alunoExists) {
+            alunoRepository.deleteById(id);
+        }
+    }
+
+}
