c2cc3039291beb721f30eee7d328a3d1a3793218
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-02-17 19:34:25 -0300

    Criação da tabela tb_alunos com colunas para cada atributo  mais Id gerado automaticamente

--- src/main/java/com/miguelfazio/tutoria/database/entity/Aluno.java
+++ src/main/java/com/miguelfazio/tutoria/database/entity/Aluno.java
@@ -1,14 +1,27 @@
 package com.miguelfazio.tutoria.database.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_alunos")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Aluno {
+
+    @Column(name = "nome", nullable = false)
     private String nome;
 
-    public Aluno() {
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
     public Aluno(String nome) {
+        this.nome = nome;
     }
 }
