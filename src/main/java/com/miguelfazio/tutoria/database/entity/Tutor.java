25b6e6de9a98bd8e820772a4286ae445c44313bd
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-02-19 13:59:35 -0300

    Criação da entidade Tutor com sua tabela ee atributos

--- src/main/java/com/miguelfazio/tutoria/database/entity/Tutor.java
+++ src/main/java/com/miguelfazio/tutoria/database/entity/Tutor.java
@@ -1,14 +1,28 @@
 package com.miguelfazio.tutoria.database.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_tutores")
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Tutor {
-    private String nome;
-    private String especialidade;
 
-    public Tutor(){
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "especialidade", nullable = false)
+    private String especialidade;
 
     public Tutor(String nome, String especialidade) {
         this.nome = nome;
