7569223589f00fc5b929918ea9fa35f2965c0c87
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-02-21 19:06:47 -0300

    Criação da entidade Agenda para ser tratada dentro do banco de dados

--- src/main/java/com/miguelfazio/tutoria/database/entity/Agenda.java
+++ src/main/java/com/miguelfazio/tutoria/database/entity/Agenda.java
@@ -1,20 +1,44 @@
 package com.miguelfazio.tutoria.database.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
+import java.util.UUID;
 
+@Entity
+@Table(name = "tb_agendas")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Agenda {
-    private Aluno aluno;
-    private Tutor tutor;
-    private Date data;
-    private String status;
-    private String tema;
-    private String descricao;
 
-    public Agenda(){
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "aluno_id", nullable = false)
+    private Aluno aluno;
+
+    @ManyToOne
+    @JoinColumn(name = "tutor_id", nullable = false)
+    private Tutor tutor;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date data;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private String tema;
+
+    @Column(nullable = false, length = 500)
+    private String descricao;
 
     public Agenda(Aluno aluno, Tutor tutor, Date data, String status, String tema, String descricao) {
         this.aluno = aluno;
