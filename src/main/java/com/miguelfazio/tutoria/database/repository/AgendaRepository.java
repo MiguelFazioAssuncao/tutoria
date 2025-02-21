9cd1a3ad02e1c7093ea3e264d441617ca9fc4ac3
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-02-21 19:48:00 -0300

    Refatorado o AgendaRepository para incluir métodos que retornam agendamentos futuros de alunos e tutores. Adicionados métodos findByAlunoIdAndDataAfterOrderByDataAsc e findByTutorIdAndDataAfterOrderByDataAsc para consultar agendamentos após a data atual.

--- src/main/java/com/miguelfazio/tutoria/database/repository/AgendaRepository.java
+++ src/main/java/com/miguelfazio/tutoria/database/repository/AgendaRepository.java
@@ -2,10 +2,18 @@ package com.miguelfazio.tutoria.database.repository;
 
 import com.miguelfazio.tutoria.database.entity.Agenda;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
-@Repository
 public interface AgendaRepository extends JpaRepository<Agenda, UUID> {
+
+    List<Agenda> findByAlunoIdOrderByDataAsc(UUID alunoId);
+
+    List<Agenda> findByTutorIdOrderByDataAsc(UUID tutorId);
+
+    List<Agenda> findByAlunoIdAndDataAfterOrderByDataAsc(UUID alunoId, LocalDateTime data);
+
+    List<Agenda> findByTutorIdAndDataAfterOrderByDataAsc(UUID tutorId, LocalDateTime data);
 }
