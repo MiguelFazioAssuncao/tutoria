b20967fb6353cb28c1b0073445251a485f553923
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-02-21 19:09:16 -0300

    Criação da entidade Material para ser gerenciada dentro do banco de dados

--- src/main/java/com/miguelfazio/tutoria/database/entity/Material.java
+++ src/main/java/com/miguelfazio/tutoria/database/entity/Material.java
@@ -1,15 +1,32 @@
 package com.miguelfazio.tutoria.database.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_materiais")
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class Material {
-    private Agenda agenda;
-    private String descricao;
-    private String caminhoArquivo;
 
-    public Material() {
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "agenda_id", nullable = false)
+    private Agenda agenda;
+
+    @Column(columnDefinition = "VARCHAR(80)",nullable = false)
+    private String descricao;
+
+    @Column(columnDefinition = "VARCHAR(255)", nullable = false)
+    private String caminhoArquivo;
 
     public Material(Agenda agenda, String descricao, String caminhoArquivo) {
         this.agenda = agenda;
